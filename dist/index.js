1d0334edc7152c2797d5fe4af71db3a6ea78bf41
kwokkan <kwokkan@users.noreply.github.com>
2020-05-22 19:34:29 +0100

    Initial commit.

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function main(...args) {
+    console.log("Running with args", ...args);
+    return 0;
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+
+function main(...args: string[]): number{
+    console.log("Running with args", ...args);
+
+    return 0;
+}
